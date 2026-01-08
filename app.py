da98f348ec6ac7b7be65e25f04fb6175f4808494
Brian <icedino2106@gmail.com>
2026-01-08 08:08:43 +0700

    update app.py

--- app.py
+++ app.py
@@ -1,10 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import random
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Halo bro, Flask sudah jalan!"
+@app.route("/", methods=["GET", "POST"])
+def index():
+    hasil = ""
+    pilihan_user = ""
+    pilihan_bot = ""
 
-if __name__ == "__main__":
-    app.run(debug=True)
+    if request.method == "POST":
+        pilihan_user = request.form["pilihan"]
+        pilihan_bot = random.choice(["batu", "gunting", "kertas"])
+
+        if pilihan_user == pilihan_bot:
+            hasil = "Seri!"
+        elif (
+            (pilihan_user == "batu" and pilihan_bot == "gunting") or
+            (pilihan_user == "gunting" and pilihan_bot == "kertas") or
+            (pilihan_user == "kertas" and pilihan_bot == "batu")
+        ):
+            hasil = "Kamu Menang!"
+        else:
+            hasil = "Kamu Kalah!"
+
+    return render_template(
+        "index.html",
+        hasil=hasil,
+        pilihan_user=pilihan_user,
+        pilihan_bot=pilihan_bot
+    )
